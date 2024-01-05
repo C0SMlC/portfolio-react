e53418dc965ecdb0f976ba34ce39a66bd19e3a0b
C0SMlC <pendurkarpratik04@gmail.com>
2024-01-05 09:54:59 +0530

    Update CSS and package.json files, and modify OverviewPage and SocialHandlesPage components

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,20 +1,77 @@
+import { Box, Text, useBreakpointValue } from "@chakra-ui/react";
+import JavascriptIcon from "../icons/JavaScriptIcon.jsx";
 import styles from "./OverviewPage.module.css";
+import HtmlIcon from "../icons/HtmlIcon.jsx";
+import CppIcon from "../icons/CppIcon.jsx";
 
 function OverviewPage() {
+  const fontSize = useBreakpointValue({ base: "xl", md: "2xl", lg: "3xl" });
+
+  const responsiveStyles = {
+    position: "absolute",
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
   return (
     <section className="container">
       <h1 className="title">
         Overview<p className="reddot">.</p>
       </h1>
-      <p>This is the overview page</p>
-      <p>And this is another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
-      <p>And this is yet another paragraph</p>
+
+      <JavascriptIcon className={`${styles.icon} ${styles.js}`} />
+      <HtmlIcon className={`${styles.icon} ${styles.html}`} />
+      <CppIcon className={`${styles.icon} ${styles.cpp}`} />
+      
+      <Box
+        maxW="3xl"
+        maxH={"3xl"}
+        borderWidth="2px"
+        rounded="xl"
+        shadow="lg"
+        py={16}
+        px={10}
+        overflow="hidden"
+        color={"#442104"}
+        bg={"#BBDEFB"}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        gap={5}
+        {...responsiveStyles}
+      >
+        <Text
+          className={styles.text}
+          fontSize={fontSize}
+          fontWeight="bold"
+          display={"flex"}
+          alignItems={"center"}
+          letterSpacing={"widest"}
+        >
+          <p className={styles.number}>01</p> &lt; Hi, I&apos;m Pratik Pendurkar
+          &gt;
+        </Text>
+
+        <Text
+          className={styles.text}
+          fontSize={fontSize}
+          fontWeight="bold"
+          display={"flex"}
+          letterSpacing={"widest"}
+        >
+          <p className={styles.number}>02</p> &lt; I Design & Build Web &gt;
+        </Text>
+
+        <Text
+          className={styles.text}
+          fontSize={fontSize}
+          fontWeight="bold"
+          display={"flex"}
+          letterSpacing={"widest"}
+        >
+          <p className={styles.number}>03</p> &lt; Applications &gt;
+        </Text>
+      </Box>
     </section>
   );
 }
--- src/pages/SocialHandlesPage.jsx
+++ src/pages/SocialHandlesPage.jsx
@@ -1,5 +1,20 @@
-function SocialHandlesPage() {
-  return <div>SocialHandles Page</div>;
+function ProjectsPage() {
+  return (
+    <section className="container">
+      <h1 className="title">
+        Socials<p className="reddot">.</p>
+      </h1>
+      <p>This is the socilas page</p>
+      <p>And this is another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+      <p>And this is yet another paragraph</p>
+    </section>
+  );
 }
 
-export default SocialHandlesPage;
+export default ProjectsPage;
