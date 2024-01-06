2565172210e0d699527d6d5f3db94f92c629519c
C0SMlC <pendurkarpratik04@gmail.com>
2024-01-06 23:50:45 +0530

    Delete unused icons and update CSS styles

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,8 +1,13 @@
-import { Box, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Img, Text, useBreakpointValue } from "@chakra-ui/react";
 import JavascriptIcon from "../icons/JavaScriptIcon.jsx";
 import styles from "./OverviewPage.module.css";
 import HtmlIcon from "../icons/HtmlIcon.jsx";
 import CppIcon from "../icons/CppIcon.jsx";
+import CSSIcon from "../icons/CssIcon.jsx";
+import ReactIcon from "../icons/ReactIcon.jsx";
+import NodeJsIcon from "../icons/NodeJsIcon.jsx";
+import MongodbIcon from "../icons/MongodbIcon.jsx";
+import Image from "../assets/HomeScreenIcon-1.png";
 
 function OverviewPage() {
   const fontSize = useBreakpointValue({ base: "xl", md: "2xl", lg: "3xl" });
@@ -18,11 +23,25 @@ function OverviewPage() {
       <h1 className="title">
         Overview<p className="reddot">.</p>
       </h1>
+      {/* <Img
+        src={Image}
+        alt="Home Screen Icon"
+        width={"350px"}
+        height={"350px"}
+        className={styles.homeScreenImage}
+      /> */}
 
       <JavascriptIcon className={`${styles.icon} ${styles.js}`} />
       <HtmlIcon className={`${styles.icon} ${styles.html}`} />
+      <CSSIcon className={`${styles.icon} ${styles.css}`} />
       <CppIcon className={`${styles.icon} ${styles.cpp}`} />
-      
+
+      <div className={styles.bottom_icons}>
+        <ReactIcon className={`${styles.icon} ${styles.react}`} />
+        <MongodbIcon className={`${styles.icon} ${styles.mongodb}`} />
+        <NodeJsIcon className={`${styles.icon} ${styles.nodejs}`} />
+      </div>
+
       <Box
         maxW="3xl"
         maxH={"3xl"}
